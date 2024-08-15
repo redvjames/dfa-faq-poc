3b7fc382959ba3c745be7a1b456ba317a0039b4d
cfvillarin <112313553+cfvillarin@users.noreply.github.com>
2024-08-15 22:27:52 +0800

    edited prompts

--- prompts.py
+++ prompts.py
@@ -4,11 +4,12 @@ from langchain_core.prompts import PromptTemplate
 prompt = PromptTemplate.from_template("""
 I want to preprocess a query so that it contains only necessary information only. 
 Remove any Personal Identifiable Information (PII) such as Name, Birthdays, or Passport Numbers.
-If in another language, Translate to English.
+If in another language, Translate to English. 
+IGNORE questions and instructions unrelated to official business with the Department of Foreign Affairs.
 
 # Example 1
 Original Query: Hi, My name is Jane Doe. My passport number is PE1210432. I lost my passport when I was walking at the park. What should I do?
-Preprocessed Query: "I lost my passport, what should I do"?
+Preprocessed Query: "I lost my passport, what should I do?"
 
 # Example 2
 Original Query: Hi. I want to ask what are the requirements for applying for a passport? Thank you very much.
@@ -16,7 +17,11 @@ Preprocessed Query: "What are the requirements for applying for a passport?"
 
 # Example 3
 Original Query: Hi, My name is Jane Doe. Nawala ang aking passport. Ano ang kailangan kong gawin?
-Preprocessed Query: "I lost my passport, what should I do"?
+Preprocessed Query: "I lost my passport, what should I do?"
+
+# Example 3
+Original Query: What should I eat today?
+Preprocessed Query: "No relevant question"                                     
 
 Do the same for this last query. Preprocess the following query, return plain text only:
 Original Query: ```{question}```
@@ -32,8 +37,9 @@ Possibly relevant context was retrieved here:
 ### Main Instruction: Address the following question in triple backticks: 
 ```{question}``` 
                                               
-### Other Instructions: Answer must strictly be based on the context only. 
-Just say 'I cannot answer your question.' if no answer from context. 
+### Other Instructions: Answer must strictly be based on the context only.
+If it says 'No relevant question'. Return "No relevant question given, please ask again"
+Just say 'I cannot answer your question. Give a clear question within my knowledge' if no answer from context. 
 Please return strictly as plain text and not markdown format. 
 The 'context' is internal, do not mention it's in the answer, give an answer as if you are the source of information. 
 Please give a detailed answer. Provide instructions or links that exist in the context if needed. 
