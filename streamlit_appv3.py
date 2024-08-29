9fe66acb9f45162ac9e401e35a91f789286bdd73
James Verdan <112069712+redvjames@users.noreply.github.com>
2024-08-29 12:58:18 +0800

    test

--- streamlit_appv3.py
+++ streamlit_appv3.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timezone, timedelta
 import time
-from streamlit_gsheets import GSheetsConnection
+# from streamlit_gsheets import GSheetsConnection
 
 # Create columns for the title and logo
 col1, col2 = st.columns([3.5, 1])  # Adjust the ratio as needed
