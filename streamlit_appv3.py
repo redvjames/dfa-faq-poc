27135a89aa5a944741d2d7a837c8be8e28d58bff
James Verdan <112069712+redvjames@users.noreply.github.com>
2025-02-22 19:45:38 +0800

    Update streamlit_appv3.py

--- streamlit_appv3.py
+++ streamlit_appv3.py
@@ -43,7 +43,7 @@ if uploaded_file is not None:
     with col1:
         # Can be used wherever a "file-like" object is accepted:
         image = uploaded_file.read()
-        st.image(image, width=500)
+        st.image(image, width=450)
 
     with col2:
         if st.button('Screen Image'):
